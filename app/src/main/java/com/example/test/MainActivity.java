cb7fa7a3b4a7d200ba087e2fd65995b473e0a781
Nikolashaaaa <mender964@gmail.com>
2023-08-15 12:33:59 +0300

    Toast Message and AlertDialog

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.test;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        Button btn_second = findViewById(R.id.btn_second);
+        btn_second.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInfo(textView.getText().toString(), btn_second);
+                showInfoAlert("Do you want close app?");
+            }
+        });
+    }
+
+    public void btnClick(View v) {
+        showInfo(((Button)v).getText().toString(), ((Button) v));
+    }
+
+    private void showInfoAlert(String text) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Big clue")
+                .setMessage(text)
+                .setCancelable(false)
+                .setPositiveButton("Of course", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void showInfo(String text, Button btn) {
+        btn.setText("You already click");
+        btn.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+
+    }
+}
