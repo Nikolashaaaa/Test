342110b57012da7fb700cf85947dfc8f3137746b
Nikolashaaaa <mender964@gmail.com>
2023-08-22 11:42:51 +0300

    Fragments Tutorial

--- app/src/main/java/com/example/test/SecondActivity.java
+++ app/src/main/java/com/example/test/SecondActivity.java
@@ -1,17 +1,51 @@
 package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Button btn_fragment1, btn_fragment2;
+    private FirstFragment firstFragment = new FirstFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        btn_fragment1 = findViewById(R.id.btn_fragment1);
+        btn_fragment2 = findViewById(R.id.btn_fragment2);
+
+        setNewFragment(firstFragment);
+
+        btn_fragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setNewFragment(firstFragment);
+            }
+        });
+
+        btn_fragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SecondFragment secondFragment = new SecondFragment();
+                setNewFragment(secondFragment);
+            }
+        });
+    }
+
+    private void setNewFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frame_layout, firstFragment);
+        ft.addToBackStack(null);
+        ft.commit();
     }
 
     public void goBack(View v) {
